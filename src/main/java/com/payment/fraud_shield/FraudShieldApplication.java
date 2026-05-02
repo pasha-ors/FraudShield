4046bdd4d76cc4d4ea39001aa6e994d4443d8540
Pavlo <pavlo.orsahosh05@gmail.com>
2026-05-02 23:50:30 +0200

    init commit

---
+++ src/main/java/com/payment/fraud_shield/FraudShieldApplication.java
@@ -0,0 +1,13 @@
+package com.payment.fraud_shield;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudShieldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FraudShieldApplication.class, args);
+	}
+
+}
